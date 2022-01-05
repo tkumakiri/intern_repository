321d5e2b8247214c0a14bd712bf3a2e040d0d4cf
statiolake <statiolake@gmail.com>
2022-01-05 15:05:11 +0900

    feat: configure project

--- backend/apiserver/settings.py
+++ backend/apiserver/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-&=z9so273pi+4i83@32ublktwkdrc@%+azd_)x4+)xrlgh31b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
