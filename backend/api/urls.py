3b1e69ff42a7b60da917d90e8eb9041965585098
omochi8h <omochi8h@gmail.com>
2022-01-07 15:19:35 +0900

    0107 login,register,ユーザ情報

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import include, url
+from django.urls import path
 from rest_framework import routers
-from .views import AuthRegister, AuthInfoGetView, AuthInfoUpdateView
+from .views import AuthRegister, AuthInfoGetView, AuthInfoUpdateView, UserRetrieve
 
 urlpatterns = [
-    url(r'^users/$', AuthRegister.as_view()),
-    url(r'^me/$', AuthInfoGetView.as_view()),
-    url(r'^auth_update/$', AuthInfoUpdateView.as_view()),
+    path('users/', AuthRegister.as_view()),
+    path('auth/me/', AuthInfoGetView.as_view()),
+    path('auth/auth_update/', AuthInfoUpdateView.as_view()),
+    path('users/<int:pk>/', UserRetrieve.as_view()),
+
 ]
--- backend/apiserver/urls.py
+++ backend/apiserver/urls.py
@@ -20,7 +20,7 @@ from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^login/', obtain_jwt_token),
-    url(r'^auth/', include('api.urls'))
+    path('login/', obtain_jwt_token),
+    path('', include('api.urls')),
 
 ]
