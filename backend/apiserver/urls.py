5e076b08e670fa3b1c3f3f6b8e935b76511d20b8
omochi8h <omochi8h@gmail.com>
2022-01-07 16:13:58 +0900

    0107 url

--- backend/apiserver/urls.py
+++ backend/apiserver/urls.py
@@ -20,7 +20,7 @@ from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', obtain_jwt_token),
+    path('auth/login/', obtain_jwt_token),
     path('', include('api.urls')),
 
 ]
