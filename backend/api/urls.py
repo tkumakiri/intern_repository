e7b9d06cd57192f1116c54e56b33b09bc8f79c14
statiolake <statiolake@gmail.com>
2022-01-08 02:11:01 +0900

    refactor: rename views_follow to views_follows

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from rest_framework import routers
 
 from api import views_directmessages
-from . import views_auth, views_lives, views_posts, views_follow, views_goods
+from . import views_auth, views_follows, views_lives, views_posts, views_goods
 
 urlpatterns = [
     path('users/', views_auth.AuthRegister.as_view()),
@@ -13,8 +13,8 @@ urlpatterns = [
     path('lives', views_lives.LivesView.as_view()),
     path('lives/<int:pk>', views_lives.LiveView.as_view()),
     path('live_registrations', views_lives.LiveRegistrationView.as_view()),
-    path('follows', views_follow.FollowsView.as_view()),
-    path('follows/<int:pk>', views_follow.FollowView.as_view()),
+    path('follows', views_follows.FollowsView.as_view()),
+    path('follows/<int:pk>', views_follows.FollowView.as_view()),
     path('posts', views_posts.PostsView.as_view()),
     path('posts/<int:pk>', views_posts.PostView.as_view()),
     path('goods', views_goods.GoodsView.as_view()),
