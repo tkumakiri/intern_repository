3fde8cb488827210780a76da1220fc224860c350
neru829 <neru829@gmail.com>
2022-01-07 18:34:44 +0900

    [fix]ファイル名の先頭を大文字に修正

--- frontend/ooturu/src/App.vue
+++ frontend/ooturu/src/App.vue
@@ -4,14 +4,14 @@
 
     <v-main>
       <!-- <HelloWorld /> -->
-      <search />
-      <dmList />
-      <LiveDetail />
+      <!-- <Search /> -->
+      <!-- <DmList /> -->
+      <!-- <LiveDetail /> -->
       <!-- <Myprofile /> -->
-      <home />
-      <login />
-      <register />
-      <liveRegister />
+      <!-- <Home />
+      <Login />
+      <Register />
+      <LiveRegister /> -->
       <v-content>
         <router-view />
       </v-content>
@@ -21,26 +21,26 @@
 
 <script>
 // import HelloWorld from "./components/HelloWorld";
-import LiveDetail from "./components/LiveDetail";
-import login from "./components/login";
-import register from "./components/register";
-import home from "./components/home";
-import liveRegister from "./components/liveRegister"
-import dmList from "./components/dmList";
-import search from "./components/search";
+//import LiveDetail from "./components/LiveDetail";
+// import login from "./components/Login";
+// import register from "./components/Register";
+// import home from "./components/Home";
+// import liveRegister from "./components/LiveRegister"
+// import dmList from "./components/DmList";
+// import search from "./components/Search";
 
 export default {
   name: "App",
 
   components: {
     // HelloWorld,
-    LiveDetail,
-    login,
-    register,
-    home,
-    liveRegister,
-    dmList,
-    search
+    // LiveDetail,
+    // Login,
+    // Register,
+    // Home,
+    // LiveRegister,
+    // DmList,
+    // Search
 },  
 
   data: () => ({
