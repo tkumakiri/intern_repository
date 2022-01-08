c9fda683c4ebc4f6676d67d45f075269ed11a7c1
statiolake <statiolake@gmail.com>
2022-01-08 11:23:31 +0900

    refactor: remove duplicated DATABASE table

--- backend/apiserver/settings.py
+++ backend/apiserver/settings.py
@@ -82,17 +82,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'apiserver.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
@@ -153,7 +142,14 @@ REST_FRAMEWORK = {
     'EXCEPTION_HANDLER': 'api.errors.custom_exception_handler',
 }
 
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # },
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'djangogirls',
