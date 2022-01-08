3e6fd777af706e683d4b3cfb5d7391adfccaa7f7
neru829 <neru829@gmail.com>
2022-01-08 15:04:22 +0900

    fix 遷移

--- frontend/ooturu/src/components/Home.vue
+++ frontend/ooturu/src/components/Home.vue
@@ -9,7 +9,7 @@
             >
                 <v-card width="640px" class="my-2 py-2 mx-auto">
                     <v-list-item-content>
-                        <v-list-item-title v-text="live.start_at + ' ' + live.title"></v-list-item-title>
+                        <v-list-item-title v-text="live.date + ' ' + live.title"></v-list-item-title>
                         <v-list-item-subtitle v-text="live.detail + '人が視聴予定です'"></v-list-item-subtitle>
                     </v-list-item-content>
                 </v-card>
@@ -27,7 +27,17 @@ export default {
     data() {
         return{
             lives: [
-]
+                {
+                    date: '2021-1-6',
+                    title: 'XXXXライブ',
+                    detail: '999'
+                },
+                {
+                    date: '2021-1-7',
+                    title: '〇〇ライブ',
+                    detail: '0'
+                }
+            ]
         }
     },
     mounted() {
